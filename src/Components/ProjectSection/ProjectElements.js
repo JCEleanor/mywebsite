687c2a799685f8909b57b238213d044e5b6753ae
JCEleanor <crcky4826@gmail.com>
2021-06-26 12:01:55 +0800

    fix stupid typo

--- src/Components/ProjectSection/ProjectElements.js
+++ src/Components/ProjectSection/ProjectElements.js
@@ -50,7 +50,7 @@ export const ProjectCard = styled.div`
 	transition: all 0.2s ease-in-out;
 
 	&:hover {
-		transfor: scale(1.02);
+		transform: scale(1.02);
 		transition: all 0.2s ease-in-out;
 		cursor: pointer;
 	}
