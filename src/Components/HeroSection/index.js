cb77a08b8bd639d789e71f51abb12749ce7f9b87
JCEleanor <crcky4826@gmail.com>
2024-10-28 16:15:40 +0800

    update package.json

--- src/Components/HeroSection/index.js
+++ src/Components/HeroSection/index.js
@@ -1,35 +1,41 @@
-import Video from '../../videos/video.mp4'
+import Video from "../../videos/video.mp4";
 
 import {
-	HeroContainer,
-	HeroBg,
-	VideoBg,
-	HeroContent,
-	HeroH1,
-	HeroP,
-	HeroBtnWrapper,
-} from './HeroElements'
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+} from "./HeroElements";
 
-import {ArrowDown, NavLinks} from '../ButtonElement'
+import { ArrowDown, NavLinks } from "../ButtonElement";
 
 const HeroSection = () => {
+  return (
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type="./video/mp4" />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Hello, I am Jing-Chun Chiu</HeroH1>
+        <HeroP>Software engineer based in Taiwan</HeroP>
+        <HeroBtnWrapper>
+          <NavLinks
+            to="about"
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact="true"
+            offset={-80}
+          >
+            <ArrowDown isWhite={true} />
+          </NavLinks>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
 
-	return (
-		<HeroContainer>
-			<HeroBg>
-				<VideoBg autoPlay loop muted src={Video} type="./video/mp4" />
-			</HeroBg>
-			<HeroContent>
-				<HeroH1>Hello, I am Jing-Chun Chiu</HeroH1>
-				<HeroP>Software engineer based in Taiwan </HeroP>
-				<HeroBtnWrapper>
-					<NavLinks to="about" smooth={true} duration={500} spy={true} exact="true" offset={-80}>
-						<ArrowDown isWhite={true}/>
-					</NavLinks>
-				</HeroBtnWrapper>
-			</HeroContent>
-		</HeroContainer>
-	)
-}
-
-export default HeroSection
+export default HeroSection;
